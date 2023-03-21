d187fa400a91e161ad512d755fb56952e9296aa0
skytool <albert.do@aol.com>
2023-03-21 10:48:15 -0500

    always basic is the essential

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+
+const random = Math.floor(Math.random()*10);
+console.log(random);
+
+const num3 = 14;
+const num4 = 10;
+
+const mod = num3 % num4;
+
+console.log(mod);
+
+const max = Math.max(20,-34,-2,3.4,9.9999);
+console.log(max);
